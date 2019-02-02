87bd4f4d2c43a67e8dfe50f877b53453e84f8642
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-02-02 15:40:00 +0900

    initial commit

---
+++ lib/ex_orde30.ex
@@ -0,0 +1,18 @@
+defmodule ExOrde30 do
+  @moduledoc """
+  Documentation for ExOrde30.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExOrde30.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_orde30_test.exs
@@ -0,0 +1,8 @@
+defmodule ExOrde30Test do
+  use ExUnit.Case
+  doctest ExOrde30
+
+  test "greets the world" do
+    assert ExOrde30.hello() == :world
+  end
+end
